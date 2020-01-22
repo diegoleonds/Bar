33d5d312e75be5b763536301059e8e03e999986f
jmarcoslima <jmarcos663@gmail.com>
2020-01-22 10:32:43 -0300

    Login com sms funcionando

--- app/src/main/java/com/example/bar/FireBaseUIActivity.java
+++ app/src/main/java/com/example/bar/FireBaseUIActivity.java
@@ -1,6 +1,7 @@
 package com.example.bar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bar.view.MainActivity;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,19 +18,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class FireBaseUIActivity extends AppCompatActivity {
+    SharedPreferences preferences;
 
     private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firebase);
-        themeAndLogo();
-
+        setContentView(R.layout.activity_main);
+        preferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+        if (preferences.contains("logado")) {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else {
+            createSignInIntent();
+        }
     }
 
     public void createSignInIntent() {
@@ -59,7 +66,8 @@ public class FireBaseUIActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Log.e("USUARIO",user.toString());
+                adicionarPreferenceLogado(preferences);
+
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
@@ -79,54 +87,19 @@ public class FireBaseUIActivity extends AppCompatActivity {
                         // ...
                     }
                 });
-        // [END auth_fui_signout]
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("logado", true);
+        editor.commit();
     }
 
-    public void delete() {
-        // [START auth_fui_delete]
-        AuthUI.getInstance()
-                .delete(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-        // [END auth_fui_delete]
-    }
-
-    public void themeAndLogo() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_theme_logo]
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        //.setLogo(R.drawable.)      // Set logo drawable
-                        .setTheme(R.style.LoginTheme)      // Set theme
-                        .build(),
-                RC_SIGN_IN);
-        // [END auth_fui_theme_logo]
-    }
-
-    public void privacyAndTerms() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-        // [START auth_fui_pp_tos]
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .setTosAndPrivacyPolicyUrls(
-                                "https://example.com/terms.html",
-                                "https://example.com/privacy.html")
-                        .build(),
-                RC_SIGN_IN);
-        // [END auth_fui_pp_tos]
-    }
-
-    public void click(View view){
+    public void click(View view) {
         createSignInIntent();
+    }
+
+    private void adicionarPreferenceLogado(SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("logado", true);
+        editor.commit();
     }
 }
 
