9e3e758565bd813a8ac86619030ed621755009d3
Eduarda Steilein <eduarda.jen@gmail.com>
2020-01-24 09:04:02 -0300

    Resolvendo erro de Listar os bares aaaaaaaaaaaaaaaaa

--- app/src/main/java/com/example/bar/controller/Controller.java
+++ app/src/main/java/com/example/bar/controller/Controller.java
@@ -31,7 +31,6 @@ public class Controller {
         bars.notifyDataSetChanged();
     }
 
-//mmm
     public void abrirCadastro(){
 
         Intent intent = new Intent(context, CadastroBars.class);
