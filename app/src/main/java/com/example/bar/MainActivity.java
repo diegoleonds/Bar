4ccd41d4c44edab80abef1f79ce04ccea083834b
=Alan Fernandes <=alan.oliveirafer@gmail.com>
2020-01-24 11:45:34 -0300

    Falta o Design da Tela...

--- app/src/main/java/com/example/bar/MainActivity.java
+++ app/src/main/java/com/example/bar/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.bar;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.bar.Alan.BarSMS.R;
 
 public class MainActivity extends AppCompatActivity {
 
